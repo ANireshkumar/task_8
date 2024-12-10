65f806bafb5520e4b5f8f14a684e1c1287cfbdc0
Niresh <niresh1606@gmail.com>
2024-12-10 11:00:09 +0530

    REMOVED HELLO

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 
-import Hello from "./components/hello"
+
 import Other from "./components/other"
 import Dis from "./components/discription"
 
 function App(){
   return(
   <>
-  <Hello/>
+
   <Other/>
   <Dis/>
   
